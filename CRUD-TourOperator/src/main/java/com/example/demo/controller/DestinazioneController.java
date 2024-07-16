309d26e69bafc53aa0abb9b33a8c3a7be8b03c42
ciroa <ciroa@DESKTOP-61FLETC.homenet.telecomitalia.it>
2024-07-16 12:21:53 +0200

    2

--- CRUD-TourOperator/src/main/java/com/example/demo/controller/DestinazioneController.java
+++ CRUD-TourOperator/src/main/java/com/example/demo/controller/DestinazioneController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,17 @@ import com.example.demo.repository.DestinazioneRepository;
 @RequestMapping("/destinations")
 public class DestinazioneController {
 
-	@Autowired
-	private DestinazioneRepository destinazioneRepository;
-	
-	@GetMapping
-	public List<Destinazione> getAllDestinazioni() {
-		return destinazioneRepository.findAll();
+    @Autowired
+    private DestinazioneRepository destinazioneRepository;
+    
+    @GetMapping
+    public List<Destinazione> getAllDestinazioni() {
+        return destinazioneRepository.findAll();
+    }
+    
+	@PostMapping
+	public Destinazione createCategory(@RequestBody Destinazione destinazione) {
+		return destinazioneRepository.save(destinazione);
 	}
 }
+
